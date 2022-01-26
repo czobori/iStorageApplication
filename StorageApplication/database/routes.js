595d0db165fcbd7e81c8e05400c5c14ef4c2772e
czobori <effi011123@gmail.com>
2022-01-26 13:19:55 +0100

    database connect

---
+++ StorageApplication/database/routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'istorage'
+});
+
+const app = express();
+
+app.get('/user',function(req,res){
+    connection.getConnection(function(err,connection){
+        connection.query('SELECT * FROM user',function(error,result,fields){
+            if(error) throw error;
+            res.send(result)
+        });
+    });
+});
+
+app.listen(3000,()=>{
+    console.log('Go to http://localhost:3000/user so you can see the data.');
+})
