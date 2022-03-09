d5c4e895dc402c6fc4ebcc56b31e9990e671afec
czobori <effi011123@gmail.com>
2022-03-09 10:25:12 +0100

    login controller in userinputcomp, rules,tulajdonságok

--- StorageApplication/components/input/userInputComp.js
+++ StorageApplication/components/input/userInputComp.js
@@ -1,13 +1,15 @@
-import {TextInput,View } from 'react-native';
-import { loginPageStyles } from '../../styles/screenStyles/loginStyles';
 import React from 'react';
+import {TextInput,View } from 'react-native';
+import {Controller} from 'react-hook-form'
+import { loginPageStyles } from '../../styles/screenStyles/loginStyles';
 
-const UserInput = ({value,setValue,placeholder,secureTextEntry}) =>{
+const UserInput = ({control,name,rules={},placeholder,secureTextEntry}) =>{
     return (
         <View>
-            <TextInput style={loginPageStyles.input} 
-                value = {value} onChangeText = {setValue}  secureTextEntry={secureTextEntry}
-                placeholder={placeholder} placeholderTextColor="#F4F6F7"/>
+            <Controller control={control} name={name} rules={rules} render={({field: {value,onChange,onBlur}})=>(
+                <TextInput value={value} onChangeText={onChange} onBlur={onBlur} secureTextEntry={secureTextEntry}
+                    placeholder={placeholder} placeholderTextColor="#F4F6F7" style={loginPageStyles.input}/>
+            )}/>
         </View>
     )
 }
--- StorageApplication/screens/LoginScreen.tsx
+++ StorageApplication/screens/LoginScreen.tsx
@@ -1,25 +1,30 @@
+import React, {useState} from 'react';
 import {View} from '../components/Themed';
+import {useForm,Controller} from 'react-hook-form'
 import {RootTabScreenProps} from '../types';
 import {containerStyles} from '../styles/element/containerStyle';
-import React, {useState} from 'react';
 import UserInput from '../components/input/userInputComp';
 import LoginButton from '../components/button/loginButtonComp';
 import LoginText from '../components/texts/LoginTextsComp';
 import GradientBack from '../components/linegradient/linegradent';
 
 export default function LoginScreen({ navigation }: RootTabScreenProps<'LoginScreen'>) {
-  const [username,setUsername] = useState('');
-  const [password,setPassword] = useState('');
-  const onLoginPressed = () =>{navigation.navigate("HomeScreen");}
+  const {control,handleSubmit, formState: {errors}} = useForm();
+  const onLoginPressed = (data: any) =>{
+    console.log(data)
+    // validation 
+
+    
+    //navigation.navigate("HomeScreen");
+  };
 
   return (
     <View style={containerStyles.container}>
       <GradientBack/>
       <LoginText/>
-      <UserInput placeholder="felhasználónév" value={username} setValue={setUsername} secureTextEntry={false}/>
-      <UserInput placeholder="jelszó" value={password} setValue={setPassword} secureTextEntry={true}/>
-      <LoginButton onPress={onLoginPressed}/>
+      <UserInput name="username"placeholder="felhasználónév" control={control} rules={{required:true}} secureTextEntry={false}/>
+      <UserInput name="password" placeholder="jelszó" control={control} rules={{required:true}} secureTextEntry={true}/>
+      <LoginButton onPress={handleSubmit(onLoginPressed)}/>
     </View>
   );
 }
-
