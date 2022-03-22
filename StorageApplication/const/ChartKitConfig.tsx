2845ae1fe1835c780c11f7102f2e16bc071e174c
czobori <effi011123@gmail.com>
2022-03-22 11:56:24 +0100

    chartkit config

--- StorageApplication/const/ChartKitConfig.tsx
+++ StorageApplication/const/ChartKitConfig.tsx
@@ -1,7 +1,8 @@
+import * as Colors from "../const/colors";
 export const progressCartConfig ={
-    backgroundColor: '#1e2833',
-    backgroundGradientFrom: '#DCF5FF',
-    backgroundGradientTo: '#DCF5FF',
+    backgroundColor: Colors.topnavbar,
+    backgroundGradientFrom: Colors.lightblue,
+    backgroundGradientTo: Colors.lightblue,
     decimalPlaces: 2,
     color: (opacity = 9) => `rgba(30, 40, 51, ${opacity})`,
     labelColor: (opacity = 9) => `rgba(0, 0,0, ${opacity})`,
