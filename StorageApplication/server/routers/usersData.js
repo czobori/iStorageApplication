d98e45b29aa64172ac0a4dd340117f6baf3fde16
czobori <effi011123@gmail.com>
2022-03-30 11:51:52 +0200

    users query

--- StorageApplication/server/routers/usersData.js
+++ StorageApplication/server/routers/usersData.js
@@ -3,7 +3,7 @@ var router = express.Router();
 import con from '../database';
 
 router.get('/user',function(req,res){
-    con.query('SELECT * FROM `user` order by full_name',function(error,rows,fields){
+    con.query('SELECT `id`,`username`,`full_name`,`email`,`phone_number` FROM `user` WHERE 1 ORDER BY `user`.`full_name` ASC',function(error,rows,fields){
         if(error) console.log(error);
         else res.send(rows);
     });
